017ecaf35b7427b93eb09557d7bca762af73e238
Mohammed Al Yahyai <45735173+MoAly7yaei@users.noreply.github.com>
2023-05-19 13:47:22 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import json
+
 
 NOTES = ['A','A#','B','C','C#','D','D#','E','F','F#','G','G#'] #NOTES index from 0 : 11
 
@@ -14,15 +16,27 @@ def scale_finder(n, scale_name):
     
     DISTANCE = data[scale_name].values.tolist()
     scale = str()
+    scale = ""
 
     for i in range(len(DISTANCE)):
             if n > len(NOTES) - 1:
                 n = n - len(NOTES)
-                scale = scale + NOTES[n]
+                scale = scale + NOTES[n] + " "
             else:
-                scale = scale + NOTES[n] 
+                scale = scale + NOTES[n] + " "
             n = n + int(DISTANCE[i])
 
 
             
     return scale
+
+def interval_finder(n, scale_name):
+    scale_finder(n, scale_name)
+    root = scale_finder.scale[0]
+    interval_1 = scale_finder.scale[1]
+    interval_2 = scale_finder.scale[2]
+    interval_3 = scale_finder.scale[3]
+    interval_4 = scale_finder.scale[4]
+    interval_5 = scale_finder.scale[5]
+    interval_6 = scale_finder.scale[6]
+    
--- run.py
+++ run.py
@@ -1,9 +1,24 @@
-from main import scale_finder
+from main import *
+import openai
+openai.organization = "org-Ha0wnxNbk8ahY55M08rvOFuu"
+openai.api_key = "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"
+openai.Model.list()
+
+
 i = 5
+output = []
 
 while i != 0:
+    msg = ""
     n = int(input("You have to select the root note of the scale using numbers (Read the Readme.txt file for more details): "))
     scale_name = input("You have to select the name of the scale mentioned in Readme.txt: ")
-    print(scale_finder(n, scale_name), end = "   ")
+
+    msg = "find out more about this music scale notes: "
+    msg = msg + scale_finder(n,scale_name)
+    #response = openai.ask(scale_finder(n,scale_name))
+    response = openai.Completion.create(model="text-davinci-003", prompt=msg, temperature=1, max_tokens=3000)
+
+    print(response["choices"])
+
     print()
     i -= 1
